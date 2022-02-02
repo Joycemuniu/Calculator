316ff23aa1242630ae2e58cf8e45e4b78ae08af4
Joycemuniu <muniurashi345gmail.com>
2022-02-02 04:45:34 +0300

    javascript

--- js/index.js
+++ js/index.js
@@ -2,14 +2,19 @@ class calculator{
     constructor(previousOperandTextElement,currentOperandTextElement)
     this.previousOperandTextElement=previousOperandTextElement
     this.currentOperandTextElement=currentOperandTextElement
+    this.clear()
 }
-clear(){
+clear(){ 
+    this.currentOperand=''
+    this.PreviousOperand=''
+    this.Operation=undefined
 
 }
 delete() {
 
 }
 appendNumber(number){
+    this.currentOperand = number
 
 }
 chooseoperation(operation){
@@ -19,14 +24,9 @@ compute(){
 
 }
 updateDisplay(){
+    this.currentOperandTextElement.innerText = this.currentOperand
 
 }
-
-
-
-
-
-
 
 const numberbuttons=document.querySelectorAll('[data-number]')
 const operationbuttons=document.querySelectorAll('[data-operation]')
@@ -35,3 +35,13 @@ const deletebutton=document.querySelector('[data-delete]')
 const allclearbutton=document.querySelector('[data-all-clear]')
 const previousOperandtextelement=document.querySelector('[data-previous-operand]')
 const currentOperandTextElement=document.querySelector('[data-current-operand]')
+
+const calculator = new calculator(previousOperandTextElement,currentOperandTextElement)
+numberbuttons.forEach(button => {
+    button.addEventListener('click',() => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+
+    })
+})
+
