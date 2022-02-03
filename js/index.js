a4f9712ecd2df821b3477fd8ff723901505aa366
Joycemuniu <muniurashi345gmail.com>
2022-02-03 09:16:58 +0300

    new

--- js/index.js
+++ js/index.js
@@ -1,50 +1,17 @@
-// document.write("hello");
-// alert("i am good");
+let display = document.getElementById('display');
 
-
-class calculator{
-    constructor(previousOperandTextElement,currentOperandTextElement)
-    this.previousOperandTextElement=previousOperandTextElement
-    this.currentOperandTextElement=currentOperandTextElement
-    this.clear()
- }
-clear(){ 
-     this.currentOperand=''
-    this.PreviousOperand=''
-    this.Operation=undefined
-
- }
- delete() {
-
- }
- appendNumber(number){
-     this.currentOperand = number
-
- }
- chooseoperation(operation){
-
- }
- compute(){
-
- }
- updateDisplay(){
-    this.currentOperandTextElement.innerText = this.currentOperand
-
-}
-
- const numberbuttons=document.querySelectorAll('[data-number]')
- const operationbuttons=document.querySelectorAll('[data-operation]')
- const equalsbutton=document.querySelector('[data-equals]')
- const deletebutton=document.querySelector('[data-delete]')
- const allclearbutton=document.querySelector('[data-all-clear]')
- const previousOperandtextelement=document.querySelector('[data-previous-operand]')
-const currentOperandTextElement=document.querySelector('[data-current-operand]')
-
- const calculator = new calculator(previousOperandTextElement,currentOperandTextElement)
- numberbuttons.forEach(button => {
-     button.addEventListener('click',() => {
-         calculator.appendNumber(button.innerText)
-         calculator.updateDisplay()
-
-     })
- })
+let buttons = Array.from(document.getElementsByClassName('button'));
+buttons.map( button => {
+      button.addEventListener('click', (e) => {
+          console.log('clicked');
+          console.log(e);
+          console.log(e.target.innerText);
+          console.log(e.target);
+    switch(e.target.innerText){
+                   case 'AC':
+                       display.innerText =''
+                       default:
+                           display.innerText += e.target.innerText;
+            }
+      })
+})
